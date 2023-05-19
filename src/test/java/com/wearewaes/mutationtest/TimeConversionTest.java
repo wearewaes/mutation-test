502099f8f1298294de5573a55ff3003bdfa04201
Newton dos Santos <neewrobert@gmail.com>
2023-05-19 12:07:05 +0200

    Fix mutations
    - add more tests to fix the mutations found by pitest
    - add files to gitignore

--- src/test/java/com/wearewaes/mutationtest/TimeConversionTest.java
+++ src/test/java/com/wearewaes/mutationtest/TimeConversionTest.java
@@ -3,6 +3,7 @@ package com.wearewaes.mutationtest;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 
 public class TimeConversionTest {
 
@@ -22,5 +23,18 @@ public class TimeConversionTest {
     public void testTimeConversion_09am_to_24h_format() {
         TimeConversion timeConversion = new TimeConversion();
         assertEquals("09:00:00", timeConversion.convert("09:00:00AM"));
+    }
+
+    @Test
+    public void testTimeConversionFail() {
+        TimeConversion timeConversion = new TimeConversion();
+        assertNotEquals("24:00:00", timeConversion.convert("12:00:00PM"));
+    }
+
+
+    @Test
+    public void testTimeConversion_12AM_to_00() {
+        TimeConversion timeConversion = new TimeConversion();
+        assertEquals("00:00:00", timeConversion.convert("12:00:00AM"));
     }
 }
